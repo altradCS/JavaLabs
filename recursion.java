fd304dde5105b02038588054bb5e61eac8d7c89a
Sokornnika <2022170sok@aupp.edu.kh>
2023-11-23 09:35:56 +0700

    add function
    Co-Authored-By: Dmitry Dobrynin <dd@tovarisch.engineer>

--- recursion.java
+++ recursion.java
@@ -1,15 +1,25 @@
 public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    public static void main(String[] args) {
+        // Test the fibonacci function
+        int n = 9; // Example: Find the 9th Fibonacci number
+        int result = fibonacci(n);
+        System.out.println("Fibonacci number at position " + n + ": " + result);
     }
-  
+
+    public static int fibonacci(int n) {
+        // Initial call to the recursive Fibonacci function
+        return fibonacci_recursive(n);
+    }
+
+    private static int fibonacci_recursive(int n) {
+        // Base cases: Fibonacci of 0 is 0, and Fibonacci of 1 is 1
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        // Recursive case: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
+    }
 }
-    
