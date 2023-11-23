2b2a3cd32d8eadf2a06baa09af9f6e605eaf3b0e
Reaksmey <2022112pin@aupp.edu.kh>
2023-11-23 09:15:31 +0700

    add function

--- recursion.java
+++ recursion.java
@@ -1,15 +1,16 @@
 public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    public static void main(String[] args) {
+        int n = 10; // Change this to the number you want to calculate
+        System.out.println("The " + n + "th Fibonacci number is: " + fibonacci(n));
     }
-  
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 }
-    
