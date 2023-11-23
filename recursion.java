267e263947e912fa1933319720813a5111a71fd9
LY Minh <2023230ly@aupp.edu.kh>
2023-11-23 14:00:41 +0700

    answer

--- recursion.java
+++ recursion.java
@@ -1,15 +1,36 @@
-public class recursion {
-  public static void main(String[] args) {
 
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+  public class recursion {
+    public static void main(String[] args) {
+      //call your recursion function
+      //print result
+      System.out.println(recursionMethod("Hello World!"));
     }
-  
-}
+
+      public static String recursionMethod(String str) {
+          // Base case: if the string is empty, return an empty string
+          if (str.isEmpty()) {
+              return "";
+          }
+
+          // Recursive case: concatenate the last character with the result of the recursion on the substring
+          int n = str.length();
+          char lastChar = str.charAt(n - 1);
+          String result = lastChar + recursionMethod(str.substring(0, n - 1));
+
+          return result;
+      }
+
+      
+  }
+//    public static String recursion(String str) {
+//      //implement your case algorithm
+//      String result = "";
+//      for (int i = str.length(); i < 0; i++) {
+//        result += str.charAt(i);
+//        }
+//
+//      return result;
+//    }
+//
+
     
